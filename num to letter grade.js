660c4510f96de68c2f73a6810ce95af7af3ed877
Anish Maharjan <165889818+anishbroo@users.noreply.github.com>
2024-11-15 15:09:45 +0545

    Add files via upload

---
+++ num to letter grade.js
@@ -0,0 +1,23 @@
+//Program that transform numerical grade to a letter grade
+
+let value = 79;
+
+if( value >= 90 ){
+  console.log("A");
+}
+
+else if(value >= 80){
+  console.log("B");
+}
+
+else if(value >= 70){
+  console.log("C");
+}
+
+else if(value >= 60){
+  console.log("D");
+}
+
+else{
+  console.log("F");
+}
---
+++ number neg or pos.js
@@ -0,0 +1,11 @@
+//Program that determines whether a given number is positive or negative
+
+let num1 = -8;
+
+if(num1 > 0 ){
+  console.log(num1 + " is positive number.");
+}
+
+else{
+  console.log(num1 + " is negative number.")
+}
